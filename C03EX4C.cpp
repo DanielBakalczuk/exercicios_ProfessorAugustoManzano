0749e7d7ffe6985c8781edc276d0374cc3b7657f
DanielBakalczuk <daniel.bakalczuk@gmail.com>
2022-07-25 03:27:30 -0300

    Update C03EX4C.cpp

--- C03EX4C.cpp
+++ C03EX4C.cpp
@@ -7,6 +7,24 @@ using namespace std;
 const float PI = 3.14159;
 int main(void)
 {
-//variaveis
+//Variaveis
+float VOLUME,R, ALTURA;
+//Formatação das casas decimais
+  cout<< setprecision(2);
+  cout<< setiosflags(ios::right);
+  cout<< setiosflags(ios::fixed);
+//Entradas
+  cout<< "Informe a altura da lata ";cin>>ALTURA;
+  cin.ignore(80, '\n');
+  cout<< "Informe o raio da lata ";cin>>R;
+  cin.ignore(80, '\n');
+//Processos
+  VOLUME=PI*pow(R, 2)*ALTURA;
+//Saida
+  cout<< "Volume da lata "<< "="<< VOLUME;
+  cout<< "\n \n";
+  cout<< "Pressione <Enter> para finalizar o programa ";
+  cin.get();
+  return 0;
 
 }
