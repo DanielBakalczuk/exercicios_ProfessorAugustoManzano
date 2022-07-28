bff27e5b856d310e47794331fe471adcc51217e2
DanielBakalczuk <daniel.bakalczuk@gmail.com>
2022-07-28 02:12:40 -0300

    Update C03EX4H.cpp

--- C03EX4H.cpp
+++ C03EX4H.cpp
@@ -6,7 +6,24 @@
 using namespace std;
 
 
-int main(void)
+  int main(void)
 {
-    float VOLUME,COMPRIMENTO,
+//VARIÁVEIS
+     float VOLUME,COMPRIMENTO,LARGURA, ALTURA;
+//ENTRADA
+   cout<<"Informe o comprimento da caixa ";cin>>COMPRIMENTO;
+   cin.ignore(80, '\n');
+   cout<<"Informe a largura da caixa ";cin>>LARGURA;
+   cin.ignore(80, '\n');
+   cout<<"Informe a altura da caixa ";cin>>ALTURA;
+   cin.ignore(80, '\n');
+//PROCESSAMENTO
+     VOLUME=COMPRIMENTO*LARGURA*ALTURA;
+//SAIDA
+   cout<<"Volume"<<" "<<"="<<" "<<VOLUME;
+   cout<<endl;
+   cout<<"Pressione <Enter> para finalizar o programa ";
+   cin.get();
+   return 0;
+  
 }
