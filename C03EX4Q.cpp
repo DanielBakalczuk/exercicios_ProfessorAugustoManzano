c3f7296d3aaaf3cdbc9cb90ce6c928e11f9024f2
DanielBakalczuk <daniel.bakalczuk@gmail.com>
2022-08-09 04:08:19 -0300

    Create C03EX4Q.cpp

---
+++ C03EX4Q.cpp
@@ -0,0 +1,31 @@
+//C03EX4Q.cpp
+/*Calcular a área do circunferência, solicitando o valor do raio*/
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace  std;
+
+
+ int main(void)
+{    
+//Habilitar acentuação
+ setlocale (LC_ALL,"Portuguese");
+//Constantes
+ const float PI =3.14159265;
+//Variáveis
+ float A,R;
+//Formatação casas decimais
+     cout<<setprecision(2);
+     cout<<setiosflags(ios::right);
+     cout<<setiosflags(ios::fixed);
+//Entrada
+         cout<<"Informe o valor do raio da circunferência ";cin>>R;
+//Processamento
+             A=PI*(pow(R, 2));
+//Saída
+         cout<<"A área da circunferencia é "<<A<<endl;
+         cout<<endl;
+         cout<<"Pressione <Enter> para finalizar o programa ";
+         cin.get();
+         return 0; 
+}
