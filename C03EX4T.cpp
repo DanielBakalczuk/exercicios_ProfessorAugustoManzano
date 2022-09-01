84bb2ff2d957a7bb7e9cb34a7b4166c5939f9f86
DanielBakalczuk <daniel.bakalczuk@gmail.com>
2022-09-01 12:32:20 -0300

    Create C03EX4T.cpp

---
+++ C03EX4T.cpp
@@ -0,0 +1,33 @@
+//C03EX4T.cpp
+/*Calcular  e apresentar o valor que um projétil que percorre uma distancia em quilometro e um espaço de tempo em minutos*/
+#include<iostream>
+#include<iomanip>
+#include<clocale>
+using namespace std;
+
+ int main(void)
+{
+//variáveis
+ float DISTANCIA,VELOCIDADE,TEMPO;
+//Habilitar acentuação
+ setlocale(LC_ALL,"portuguese");
+//Formatação das casas decimais
+ cout<<setprecision(2);
+ cout<<setiosflags(ios::right);
+ cout<<setiosflags(ios::fixed);
+//Entrada
+     cout<<"Informe a distancia percorrida pelo projétil em quilômetros ";cin>>DISTANCIA;
+     cin.ignore(80, '\n');
+     cout<<"Informe a o tempo em minutos que o projetil demorou para percorrer o trajeto completo ";cin>>TEMPO;
+     cin.ignore(80, '\n');
+//Processamento
+         VELOCIDADE=(DISTANCIA*1000)/(TEMPO*60);
+//Saída
+     cout<<"A velocidade do projétil é de "<<" "<<VELOCIDADE<<" Metros por segundo"<<endl;
+     cout<<endl;
+     cout<<"pressione <Enter> para finalizar o programa";
+     cin.get();
+return 0;
+
+}
+
