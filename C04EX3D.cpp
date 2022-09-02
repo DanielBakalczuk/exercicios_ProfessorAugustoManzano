dd2fc602631b1d64b378ace0c263cc8667fefe03
DanielBakalczuk <daniel.bakalczuk@gmail.com>
2022-09-01 21:04:12 -0300

    Update C04EX3D.cpp

--- C04EX3D.cpp
+++ C04EX3D.cpp
@@ -1,4 +1,4 @@
-//C04EX3D.cpp
+//C04EX3.cpp
 /*ler 4 notas e tirar a media aritimetica, caso a media seja maior ou igual a 7 apresentar a msg de APROVADO
 caso for menor, pedir a entrada de uma quinta nota(recuperação) e calcular uma nova media aritimetica entre A primeira media e a nota do exame final,
  se essa media for maior ou igual a 5 apresentar a msg "aprovado em exame", caso contrario apresentar a msg "reprovado" */
@@ -26,22 +26,29 @@ using namespace std;
 //Decisão
      if(MD>=7)
      {
-     cout<<"Aprovado";
+      cout<<"Aprovado";
+      cin.ignore(80, '\n');
+      cout<<endl;
+      cout<<"Pressione <Enter> para finalizar o programa";
+      cin.get();
+return 0;
      }
      else
      {
-     cout<<"Informe a nota do exame ";cin>>N5;
-     cin.ignore(80, '\n');
+      cout<<"Informe a nota do exame ";cin>>N5;
+      cin.ignore(80, '\n');
          MD2=(N5+MD)/2;
 
      }
       if(MD2>=5)
       {
-         cout<<"Aprovado em exame final ";
+       cout<<"Aprovado em exame final ";
+       cin.ignore(80, '\n');
       }
       else
       {
-         cout<<"Reprovado";
+       cout<<"Reprovado";
+       cin.ignore(80, '\n');
       }
 
 //Saida
